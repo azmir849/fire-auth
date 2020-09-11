2403b3738ef9483fc85db712ebc4ec7b0fb9de07
azmir849 <azmir15-849@diu.edu.bd>
2020-09-11 11:39:28 +0600

    fire-auth signIn and signOut done

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALeoLuMwEn9-XKDuTtC8rv4yVvBQb7dsM",
+    authDomain: "ema-john-2d08a.firebaseapp.com",
+    databaseURL: "https://ema-john-2d08a.firebaseio.com",
+    projectId: "ema-john-2d08a",
+    storageBucket: "ema-john-2d08a.appspot.com",
+    messagingSenderId: "231601188048",
+    appId: "1:231601188048:web:06fec31a946981484c42d5"
+  };
+
+export default firebaseConfig;
